197fc1f78648e6727775020734620fca766be11f
midnight1701 <laml69087@gmail.com>
2025-11-23 14:11:15 +0800

    Checkpoint 2 - Completed

--- src/scenes/battle_scene.py
+++ src/scenes/battle_scene.py
@@ -20,6 +20,7 @@ class BattleScene(Scene):
         self.background = BackgroundSprite("backgrounds/background1.png")
         self.font = pg.font.Font("assets/fonts/PixeloidSans.ttf", size=22)
         self.box_font = pg.font.Font("assets/fonts/PixeloidSans.ttf", size=25)
+        self.alt_font = pg.font.Font("assets/fonts/Minecraft.ttf", size=20)
 
         # Dialogue setup
         self.dialogue_rect = pg.Rect(0, 0, GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT * 0.2)
@@ -59,6 +60,46 @@ class BattleScene(Scene):
         self.ally_template = pg.transform.scale(template, (300, 90))
         self.enemy_template = pg.transform.scale(template, (300, 90))
 
+        self.ally_info_img = resource_manager.get_image(self.ally_info[len(self.ally_info) - 1])
+        self.ally_info_img = pg.transform.scale(self.ally_info_img, (75, 75))
+        self.ally_info_img_rect = pg.Rect(self.ally_info_rect.topleft[0] + 12, self.ally_info_rect[1] - 3, 75, 75)
+        self.ally_name = self.alt_font.render(self.battle.player.name, True, (0, 0, 0))
+        self.ally_name_width, self.ally_name_height = self.ally_name.get_width(), self.ally_name.get_height()
+
+        self.ally_name_rect = pg.Rect(self.ally_info_img_rect.topright[0] + 10, self.ally_info_img_rect.topright[1] + 22, self.ally_name_width, self.ally_name_height)
+        self.ally_level = self.alt_font.render(f"Lv {self.battle.player.level}", True, (0, 0, 0))
+        self.ally_level_rect = pg.Rect(self.ally_name_rect.topright[0] + 30, self.ally_name_rect.topright[1], self.ally_name_width, self.ally_name_height)
+        self.hp_rect = pg.Rect(self.ally_name_rect.bottomleft[0], self.ally_name_rect.bottomright[1] + 8, self.ally_name_width + 15, 15)
+        self.red_hp = pg.transform.scale(resource_manager.get_image("UI/UI_Flat_BarFill01c.png"), (self.ally_name_width + 15, 15))
+        self.green_hp = pg.transform.scale(resource_manager.get_image("UI/UI_Flat_BarFill01a.png"),
+                                           (((self.ally_name_width + 15) *
+                                             (self.battle.player.hp / self.battle.player.max_hp)), 15))
+        self.hp_text = self.alt_font.render(f"{self.battle.player.hp}/{self.battle.player.max_hp}", True,
+                                            (0, 0, 0))
+        self.hp_text_width, self.hp_text_height = self.hp_text.get_width(), self.hp_text.get_height()
+        self.hp_text_rect = pg.Rect(self.ally_level_rect.bottomleft[0], self.hp_rect.topright[1], self.hp_text_width, self.hp_text_height)
+
+        self.enemy_info_img = resource_manager.get_image(self.enemy_info[len(self.enemy_info) - 1])
+        self.enemy_info_img = pg.transform.scale(self.enemy_info_img, (75, 75))
+        self.enemy_info_img_rect = pg.Rect(self.enemy_info_rect.topleft[0] + 12, self.enemy_info_rect[1] - 3, 75, 75)
+        self.enemy_name = self.alt_font.render(self.battle.enemy.name, True, (0, 0, 0))
+        self.enemy_name_width, self.enemy_name_height = self.enemy_name.get_width(), self.enemy_name.get_height()
+        self.enemy_name_rect = pg.Rect(self.enemy_info_img_rect.topright[0] + 10, self.enemy_info_img_rect.topright[1] + 22, self.enemy_name_width, self.enemy_name_height)
+        self.enemy_level = self.alt_font.render(f"Lv {self.battle.enemy.level}", True, (0, 0, 0))
+        self.enemy_level_rect = pg.Rect(self.enemy_name_rect.topright[0] + 30, self.enemy_name_rect.topright[1],
+                                       self.enemy_level.get_width(), self.enemy_level.get_height())
+        self.enemy_hp_rect = pg.Rect(self.enemy_name_rect.bottomleft[0], self.enemy_name_rect.bottomright[1] + 8,
+                               self.enemy_name.get_width() + 15, 15)
+        self.enemy_hp_text = self.alt_font.render(f"{self.battle.enemy.hp}/{self.battle.enemy.max_hp}", True,
+                                            (0, 0, 0))
+        self.enemy_green_hp = pg.transform.scale(resource_manager.get_image("UI/UI_Flat_BarFill01a.png"),
+                                           (((self.enemy_name_width + 15) *
+                                             (self.battle.enemy.hp / self.battle.enemy.max_hp)), 15))
+        self.enemy_hp_width, self.enemy_hp_height = self.enemy_hp_text.get_width(), self.enemy_hp_text.get_height()
+        self.enemy_hp_text_rect = pg.Rect(self.enemy_level_rect.bottomleft[0], self.enemy_hp_rect.topright[1],
+                                    self.enemy_hp_width, self.enemy_hp_height)
+
+
 
         # Button in battle scene
         self.offset = 200
@@ -90,19 +131,55 @@ class BattleScene(Scene):
         self.ally_monster = [random.choice(scene_manager._scenes["game"].game_manager.bag._monsters_data)]
         self.enemy_info = self.monster_info(self.enemy_monster)
         self.ally_info = self.monster_info(self.ally_monster)
+
+        self.ally_info_img = resource_manager.get_image(self.ally_info[len(self.ally_info) - 1])
+        self.ally_info_img = pg.transform.scale(self.ally_info_img, (75, 75))
+
+        self.enemy_info_img = resource_manager.get_image(self.enemy_info[len(self.enemy_info) - 1])
+        self.enemy_info_img = pg.transform.scale(self.enemy_info_img, (75, 75))
+
+
         self.enemy_monster_img = pg.transform.scale(
             resource_manager.get_image(self.enemy_info[len(self.enemy_info) - 1]), (250, 250))
         self.ally_monster_img = pg.transform.flip(
             pg.transform.scale(resource_manager.get_image(self.ally_info[len(self.ally_info) - 1]), (250, 250)),
             flip_x=True, flip_y=False)
         self.battle = BattleSystem(self.ally_info, self.enemy_info, True) if scene_manager.monster_catch else BattleSystem(self.ally_info, self.enemy_info, False)
-
+        self.ally_name = self.alt_font.render(self.battle.player.name, True, (0, 0, 0))
+        self.enemy_name = self.alt_font.render(self.battle.enemy.name, True, (0, 0, 0))
+        self.ally_level = self.alt_font.render(f"Lv {self.battle.player.level}", True, (0, 0, 0))
+        self.enemy_level = self.alt_font.render(f"Lv {self.battle.enemy.level}", True, (0, 0, 0))
+        self.ally_name_width, self.ally_name_height = self.ally_name.get_width(), self.ally_name.get_height()
+        self.hp_text_width, self.hp_text_height = self.hp_text.get_width(), self.hp_text.get_height()
+        self.enemy_name_width, self.enemy_name_height = self.enemy_name.get_width(), self.enemy_name.get_height()
+        self.enemy_hp_width, self.enemy_hp_height = self.enemy_hp_text.get_width(), self.enemy_hp_text.get_height()
+        self.green_hp = pg.transform.scale(resource_manager.get_image("UI/UI_Flat_BarFill01a.png"),
+                                           (((self.ally_name.get_width() + 15) *
+                                             (self.battle.player.hp / self.battle.player.max_hp)), 15))
+        self.hp_text = self.alt_font.render(f"{self.battle.player.hp}/{self.battle.player.max_hp}", True,
+                                            (0, 0, 0))
+        self.enemy_hp_text = self.alt_font.render(f"{self.battle.enemy.hp}/{self.battle.enemy.max_hp}", True,
+                                                  (0, 0, 0))
+        self.enemy_green_hp = pg.transform.scale(resource_manager.get_image("UI/UI_Flat_BarFill01a.png"),
+                                                 (((self.enemy_name_width + 15) *
+                                                   (self.battle.enemy.hp / self.battle.enemy.max_hp)), 15))
 
     def exit(self) -> None:
         scene_manager.monster_catch = False
 
 
     def update(self, dt: float) -> None:
+        self.green_hp = pg.transform.scale(resource_manager.get_image("UI/UI_Flat_BarFill01a.png"),
+                                           (((self.ally_name.get_width() + 15) *
+                                             (self.battle.player.hp / self.battle.player.max_hp)), 15))
+        self.hp_text = self.alt_font.render(f"{self.battle.player.hp}/{self.battle.player.max_hp}", True,
+                                            (0, 0, 0))
+        self.enemy_hp_text = self.alt_font.render(f"{self.battle.enemy.hp}/{self.battle.enemy.max_hp}", True,
+                                                  (0, 0, 0))
+        self.enemy_green_hp = pg.transform.scale(resource_manager.get_image("UI/UI_Flat_BarFill01a.png"),
+                                                 (((self.enemy_name_width + 15) *
+                                                   (self.battle.enemy.hp / self.battle.enemy.max_hp)), 15))
+
         if isinstance(self.battle.state, PlayerTurn) and self.battle.state.action is None:
             self.attack_button.update(dt)
             self.defend_button.update(dt)
@@ -118,6 +195,7 @@ class BattleScene(Scene):
 
         if input_manager.key_pressed(K_ESCAPE):
             scene_manager.change_scene("game")
+
 
         self.battle.update()
 
@@ -147,21 +225,46 @@ class BattleScene(Scene):
             if self.battle.state.enemy_setup:
                 screen.blit(self.enemy_monster_img, self.enemy_monster_rect)
                 screen.blit(self.enemy_template, self.enemy_info_rect)
+                screen.blit(self.enemy_info_img, self.enemy_info_img_rect)
+                screen.blit(self.enemy_name, self.enemy_name_rect)
+                screen.blit(self.enemy_level, self.enemy_level_rect)
+                screen.blit(self.red_hp, self.enemy_hp_rect)
+                screen.blit(self.enemy_green_hp, self.enemy_hp_rect)
+                screen.blit(self.enemy_hp_text, self.enemy_hp_text_rect)
 
 
             if self.battle.state.ally_setup:
                 screen.blit(self.ally_monster_img, self.ally_monster_rect)
                 screen.blit(self.ally_template, self.ally_info_rect)
+                screen.blit(self.ally_info_img, self.ally_info_img_rect)
+                screen.blit(self.ally_name, self.ally_name_rect)
+                screen.blit(self.ally_level, self.ally_level_rect)
+                screen.blit(self.red_hp, self.hp_rect)
+                screen.blit(self.green_hp, self.hp_rect)
+                screen.blit(self.hp_text, self.hp_text_rect)
+
                 self.displayed = True
 
 
         if self.displayed:
             screen.blit(self.enemy_monster_img, self.enemy_monster_rect)
             screen.blit(self.enemy_template, self.enemy_info_rect)
-
+            screen.blit(self.enemy_info_img, self.enemy_info_img_rect)
+            screen.blit(self.enemy_name, self.enemy_name_rect)
+            screen.blit(self.enemy_level, self.enemy_level_rect)
+            screen.blit(self.red_hp, self.enemy_hp_rect)
+            screen.blit(self.enemy_green_hp, self.enemy_hp_rect)
+            screen.blit(self.enemy_hp_text, self.enemy_hp_text_rect)
 
             screen.blit(self.ally_monster_img, self.ally_monster_rect)
             screen.blit(self.ally_template, self.ally_info_rect)
+            screen.blit(self.ally_info_img, self.ally_info_img_rect)
+            screen.blit(self.ally_info_img, self.ally_info_img_rect)
+            screen.blit(self.ally_name, self.ally_name_rect)
+            screen.blit(self.ally_level, self.ally_level_rect)
+            screen.blit(self.red_hp, self.hp_rect)
+            screen.blit(self.green_hp, self.hp_rect)
+            screen.blit(self.hp_text, self.hp_text_rect)
 
 
     def monster_info(self, monster):
